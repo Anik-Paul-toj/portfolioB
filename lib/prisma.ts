52308a1cfc0f9b4aed25aa590e7f48cc95e9eff5
Anik Paul <trojanik003@gmail.com>
2026-09-07 19:23:27 +0530

    Prepare portfolio for deployment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,6 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const adapter = new PrismaLibSql({
   url: process.env.DATABASE_URL || 'file:./dev.db',
+  authToken: process.env.DATABASE_AUTH_TOKEN,
 });
 
 export const prisma =
